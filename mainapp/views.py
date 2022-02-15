d074ff61ddc3b35dfc1b766658064a5928e1190c
Maxim <maxim.averin@inbox.ru>
2022-02-15 13:48:28 +0300

    Lesson 5. Basket counter

--- mainapp/views.py
+++ mainapp/views.py
@@ -1,44 +1,9 @@
 from django.conf import settings
-from django.shortcuts import render
-from django.utils import timezone
-
-from .models import Contact, Product, ProductCategory
-
-
-def main(request):
-    title = "главная"
-
-    products = Product.objects.all()[:4]
-
-    content = {"title": title, "products": products, "media_url": settings.MEDIA_URL}
-    return render(request, "mainapp/index.html", content)
-
-
-def products(request, pk=None):
-    title = "продукты"
-    links_menu = ProductCategory.objects.all()
-    same_products = Product.objects.all()
-    content = {
-        "title": title,
-        "links_menu": links_menu,
-        "same_products": same_products,
-        "media_url": settings.MEDIA_URL,
-    }
-    if pk:
-        print(f"User select category: {pk}")
-    return render(request, "mainapp/products.html", content)
-
-
-def contact(request):
-    title = "о нас"
-    visit_date = timezone.now()
-    locations = Contact.objects.all()
-    content = {"title": title, "visit_date": visit_date, "locations": locations}
-    return render(request, "mainapp/contact.html", content)
-from django.conf import settings
 from django.shortcuts import get_object_or_404, render
 from django.utils import timezone
 
+from basketapp.models import Basket
+
 from .models import Contact, Product, ProductCategory
 
 
@@ -54,6 +19,14 @@ def main(request):
 def products(request, pk=None):
     title = "продукты"
     links_menu = ProductCategory.objects.all()
+
+    basket = []
+    if request.user.is_authenticated:
+        basket = Basket.objects.filter(user=request.user)
+        # or you can use this
+        # _basket = request.user.basket.all()
+        # print(f'basket / _basket: {len(_basket)} / {len(basket)}')
+
     if pk is not None:
         if pk == 0:
             products = Product.objects.all().order_by("price")
@@ -67,6 +40,7 @@ def products(request, pk=None):
             "category": category,
             "products": products,
             "media_url": settings.MEDIA_URL,
+            "basket": basket,
         }
         return render(request, "mainapp/products_list.html", content)
     same_products = Product.objects.all()
@@ -75,6 +49,8 @@ def products(request, pk=None):
         "links_menu": links_menu,
         "same_products": same_products,
         "media_url": settings.MEDIA_URL,
+        "same_products": same_products,
+        "basket": basket,
     }
     if pk:
         print(f"User select category: {pk}")
